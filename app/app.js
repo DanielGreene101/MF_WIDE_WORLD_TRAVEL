afe92fc02e566f5b5595342ceafb77e4dfe79d49
DanielGreene101 <d.b.greene101@gmail.com>
2017-09-05 11:31:57 -0500

    better finished copy

--- app/app.js
+++ app/app.js
@@ -2,35 +2,21 @@ console.log("Connected");
 
 var app = angular.module('bookapp', []);
 
-// app.config(function($routeProvider){
-// 	$routeProvider
-// 	.when('/',{
-// 		templateUrl: 'index.html',
-// 		controller: "bookCtrl"
-// 	});
-// });
-
-// app.factory("bookFactory", function($scope, $q, $http){
-// 	console.log("Step 1 done");
-// 	let booksBuild = () => {
-// 		let books = [];
-// 		return $q ((resolve, reject) =>{
-// 			$http.get('../../data/guides.js')
-// 			.then((data) =>{
-// 				console.log("data", data);
-// 				let dataCollection = data.data;
-// 				Object.keys(dataCollection).forEach((key) =>{
-// 					books.push(dataCollection[key]);
-// 				});
-// 				resolve(books);
-// 				console.log("books", books);
-
-// 				$scope.guides = data;
-// 			})
-// 			.catch((error)=>{
-// 				reject(error);
-// 				console.log("NO BOOKS");
-// 			});
-// 		});
-// 	}
-// });
+app.factory("bookFactory", function($q, $http){
+	console.log("Step 1 done");
+	let getBooks = () => {
+		return $q ((resolve, reject) =>{
+			$http.get('data/guides.json')
+			.then((itemObject) =>{
+				let itemCollection = itemObject.data;
+				console.log(itemObject.data);;
+				resolve(itemCollection.guides)
+			})
+			.catch((error)=>{
+				reject(error);
+				console.log("NO BOOKS");
+			});
+		});
+	};
+	return {getBooks};
+});
--- app/controllers/bookCtrl.js
+++ app/controllers/bookCtrl.js
@@ -1,7 +1,5 @@
 "use strict";
 
-var app = angular.module('bookapp', []);
-
 // app.config(function($routeProvider){
 // 	$routeProvider
 // 	.when('/',{
@@ -9,24 +7,24 @@ var app = angular.module('bookapp', []);
 // 		controller: "bookCtrl"
 // 	});
 // });
-app.factory("bookFactory", function($q, $http){
-	console.log("Step 1 done");
-	let getBooks = () => {
-		return $q ((resolve, reject) =>{
-			$http.get('data/guides.json')
-			.then((itemObject) =>{
-				let itemCollection = itemObject.data;
-				console.log(itemObject.data);;
-				resolve(itemCollection.guides)
-			})
-			.catch((error)=>{
-				reject(error);
-				console.log("NO BOOKS");
-			});
-		});
-	};
-	return {getBooks};
-});
+// app.factory("bookFactory", function($q, $http){
+// 	console.log("Step 1 done");
+// 	let getBooks = () => {
+// 		return $q ((resolve, reject) =>{
+// 			$http.get('data/guides.json')
+// 			.then((itemObject) =>{
+// 				let itemCollection = itemObject.data;
+// 				console.log(itemObject.data);;
+// 				resolve(itemCollection.guides)
+// 			})
+// 			.catch((error)=>{
+// 				reject(error);
+// 				console.log("NO BOOKS");
+// 			});
+// 		});
+// 	};
+// 	return {getBooks};
+// });
 
 app.controller("bookCtrl", function($scope, bookFactory){
 	console.log("got into controller");
