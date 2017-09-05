34d758e78ca1ec74f6d84e6bde16a40480033bcd
DanielGreene101 <d.b.greene101@gmail.com>
2017-09-05 11:18:59 -0500

    finished I believe

--- app/app.js
+++ app/app.js
@@ -1,1 +1,36 @@
 console.log("Connected");
+
+var app = angular.module('bookapp', []);
+
+// app.config(function($routeProvider){
+// 	$routeProvider
+// 	.when('/',{
+// 		templateUrl: 'index.html',
+// 		controller: "bookCtrl"
+// 	});
+// });
+
+// app.factory("bookFactory", function($scope, $q, $http){
+// 	console.log("Step 1 done");
+// 	let booksBuild = () => {
+// 		let books = [];
+// 		return $q ((resolve, reject) =>{
+// 			$http.get('../../data/guides.js')
+// 			.then((data) =>{
+// 				console.log("data", data);
+// 				let dataCollection = data.data;
+// 				Object.keys(dataCollection).forEach((key) =>{
+// 					books.push(dataCollection[key]);
+// 				});
+// 				resolve(books);
+// 				console.log("books", books);
+
+// 				$scope.guides = data;
+// 			})
+// 			.catch((error)=>{
+// 				reject(error);
+// 				console.log("NO BOOKS");
+// 			});
+// 		});
+// 	}
+// });
--- app/controllers/bookCtrl.js
+++ app/controllers/bookCtrl.js
@@ -1,10 +1,43 @@
 "use strict";
 
-var bookapp = angular.module('bookapp', []);
+var app = angular.module('bookapp', []);
 
-app.controller("bookCtrl", function($scope, $http){
-	$http.get('../data/guides.js')
-	.then((data) =>{
-		$scope.guides = data;
+// app.config(function($routeProvider){
+// 	$routeProvider
+// 	.when('/',{
+// 		templateUrl: 'index.html',
+// 		controller: "bookCtrl"
+// 	});
+// });
+app.factory("bookFactory", function($q, $http){
+	console.log("Step 1 done");
+	let getBooks = () => {
+		return $q ((resolve, reject) =>{
+			$http.get('data/guides.json')
+			.then((itemObject) =>{
+				let itemCollection = itemObject.data;
+				console.log(itemObject.data);;
+				resolve(itemCollection.guides)
+			})
+			.catch((error)=>{
+				reject(error);
+				console.log("NO BOOKS");
+			});
+		});
+	};
+	return {getBooks};
+});
+
+app.controller("bookCtrl", function($scope, bookFactory){
+	console.log("got into controller");
+	bookFactory.getBooks()
+	.then(function(itemCollection){
+		let bookArray = [];
+		let books = Object.keys(itemCollection);
+		books.forEach((book) =>{
+			bookArray.push(itemCollection[book]);
+		});
+		$scope.books = bookArray;
+		console.log("books", bookArray);
 	});
 });
